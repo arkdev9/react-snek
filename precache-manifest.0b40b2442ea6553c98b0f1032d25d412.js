a2be71d2e050c98017e07abfe586b686eb080061
Abhishek Kasireddy <akasireddy99@gmail.com>
2020-10-24 19:11:56 +0530

    Updates

---
+++ precache-manifest.0b40b2442ea6553c98b0f1032d25d412.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a6e79285e29ae18b5f0032ebd369100",
+    "url": "/react-snek/index.html"
+  },
+  {
+    "revision": "62564072c5578766d212",
+    "url": "/react-snek/static/css/main.0be03044.chunk.css"
+  },
+  {
+    "revision": "2a262f621a97f210e8c0",
+    "url": "/react-snek/static/js/2.af8413be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-snek/static/js/2.af8413be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62564072c5578766d212",
+    "url": "/react-snek/static/js/main.b52b6637.chunk.js"
+  },
+  {
+    "revision": "8bb9b5a8f9290ecd48ac",
+    "url": "/react-snek/static/js/runtime-main.efbb0e20.js"
+  }
+]);
